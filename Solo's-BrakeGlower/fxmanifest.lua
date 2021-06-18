bf2c5df49c8f43bdd1a85b22be840c34e408f833
Jordan <75797512+0fficerSolo@users.noreply.github.com>
2021-06-18 14:09:46 +0100

    Add files via upload

---
+++ Solo's-BrakeGlower/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+author '0fficerSolo#0001'
+description 'Solos Brake Glowing when you stop really quickly'
+version '1.0.0'
+
+server_scripts {
+    'server/server.lua'
+}
+
+client_scripts {
+    'client/functions.lua'
+    'client/client.lua'
+}
+
+shared_scripts {
+    'config.lua'
+}
